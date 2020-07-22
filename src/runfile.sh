8017a60be26506cbc90691fe98c8656c40dcf7d8
HaleyFogelson <haleyfogelson@gmail.com>
2020-07-22 10:38:46 -0400

    added a bash file

---
+++ src/runfile.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+docker build --tag src:1.0 .
+docker run src:1.0 https://dogtime.com/assets/uploads/2018/10/puppies-cover-1280x720.jpg
+docker run src:1.0 https://media1.popsugar-assets.com/files/thumbor/L4cUBqWQhC4Zfnqv6e7AZ9kjUpY/0x159:2003x2162/fit-in/1024x1024/filters:format_auto-!!-:strip_icc-!!-/2019/08/07/875/n/24155406/9ffb00255d4b2e079b0b23.01360060_/i/Cute-Pictures-German-Shepherd-Puppies.jpg
+docker run src:1.0 ./images/Rona.jpg upload
